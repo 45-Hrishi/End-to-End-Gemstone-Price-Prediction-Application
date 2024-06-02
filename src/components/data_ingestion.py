544a9ef4c27463f731bfd6a7314801945a661526
45-Hrishi <hrishikeshkothawade1@gmail.com>
2024-06-02 23:00:08 +0530

    Other Folders Added

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,9 @@
+from logger.logger import logging
+import os
+import sys
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from exception.exception import customexception
+from dataclasses import dataclass
+from pathlib import Path
