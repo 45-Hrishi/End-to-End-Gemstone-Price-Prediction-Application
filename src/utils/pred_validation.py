9fdc906af682076cc697d0861e97d41fb19c0ae6
45-Hrishi <hrishikeshkothawade1@gmail.com>
2024-06-09 23:26:32 +0530

    Prediction validation added

---
+++ src/utils/pred_validation.py
@@ -0,0 +1,43 @@
+from logger.logger import logging
+from exception.exception import customexception
+import pandas as pd
+import sys
+
+class CustomData:
+    def __init__(self,
+                 carat:float,
+                 depth:float,
+                 table:float,
+                 x:float,
+                 y:float,
+                 z:float,
+                 cut:str,
+                 color:str,
+                 clarity:str):
+        self.carat=carat
+        self.depth=depth
+        self.table=table
+        self.x=x
+        self.y=y
+        self.z=z
+        self.cut = cut
+        self.color = color
+        self.clarity = clarity
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'carat':[self.carat],
+                'depth':[self.depth],
+                'table':[self.table],
+                'x':[self.x],
+                'y':[self.y],
+                'z':[self.z],
+                'cut':[self.cut],
+                'color':[self.color],
+                'clarity':[self.clarity]
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info("Dataframe Created successfully")
+            return df
+        except Exception as e:
+            raise customexception(e,sys)
